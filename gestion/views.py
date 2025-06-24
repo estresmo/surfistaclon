6fb40a3d5d31b4f9f4873181a4129789dd3e90f4
Juan PC <estresmo@gmail.com>
2025-06-24 00:14:00 -0400

    arreglo en whatsapp txt

--- gestion/views.py
+++ gestion/views.py
@@ -247,6 +247,7 @@ class ComprasListView(LoginRequiredMixin, ListView):
                 "cantidad",
                 "verificado",
                 "evento__url",
+                "evento__nombre",
                 "metodo__moneda",
                 "monto_bs",
             )
