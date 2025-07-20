028e1ebc4cd7726b1f13b8076f77536889250e1c
Juan PC <estresmo@gmail.com>
2025-07-19 22:59:27 -0400

    cambio url y telefono y logo redondo

--- core/settings.py
+++ core/settings.py
@@ -125,7 +125,7 @@ CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379",
-        "KEY_PREFIX": "chipibike",
+        "KEY_PREFIX": "yeison",
     }
 }
 
